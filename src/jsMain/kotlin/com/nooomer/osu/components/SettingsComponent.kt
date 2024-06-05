bb1f86fdb2311f43c8adf2333d3b81c296eac24a
Nooomer <ampilogov.qaz@gmail.com>
2024-06-05 05:45:06 +0500

    setting screen adding value if setting already have added toast and change button color when save

--- src/jsMain/kotlin/com/nooomer/osu/components/SettingsComponent.kt
+++ src/jsMain/kotlin/com/nooomer/osu/components/SettingsComponent.kt
@@ -7,44 +7,72 @@ import io.kvision.core.Container
 import io.kvision.core.StringPair
 import io.kvision.core.onChange
 import io.kvision.form.select.select
+import io.kvision.html.ButtonStyle
+import io.kvision.html.ButtonType
 import io.kvision.html.button
 import io.kvision.html.div
 import io.kvision.state.ObservableValue
+import io.kvision.state.bind
+import io.kvision.toast.ToastContainer
+import io.kvision.toast.ToastContainerPosition
 import kotlinx.browser.window
 
 object SettingsComponent {
     private var modeList = LeaderboardsView.entries.map {
-            StringPair(it.toString(), it.name.lowercase())
-        }
+        StringPair(it.toString(), it.name.lowercase())
+    }
     private var chooises = 0
-    fun Container.settings(appState: ObservableValue<AppState>) {
-        if(!window.localStorage.getItem("defaultLeaderboardType").isNullOrEmpty()){
+    private var alreadyValueState = ObservableValue("")
 
+    private fun String.toIndex(): Int {
+        when (this) {
+            "vanila" -> return 0
+            "relax" -> return 1
+            "autopilot" -> return 2
+        }
+        return -1
+    }
+
+    fun Container.settings(appState: ObservableValue<AppState>) {
+        if (!window.localStorage.getItem("defaultLeaderboardType").isNullOrEmpty()) {
+            alreadyValueState.value = window.localStorage.getItem("defaultLeaderboardType")?.lowercase().toString()
         }
         navBar()
-        div(className = "settings d-flex") {
-            div(className = "settings d-flex justify-content-center"){
+        div(className = "settings d-flex align-items-center justify-content-center flex-column") {
+            div(className = "settings d-flex flex-column justify-content-center").bind(alreadyValueState) {
                 select(
-                    modeList,
+                    value = alreadyValueState.value,
+                    options = modeList,
                     label = "Default leaderboard type",
-                ).onChange{
-                    chooises = selectedIndex
+                ) {
+                    this.selectedIndex = alreadyValueState.value.toIndex()
+                }.onChange {
+                    chooises = this.selectedIndex
                 }
-                button(className = "settings d-float-right", text = "Save").onClick {
-                    when(chooises){
-                        0 ->{
+                button(className = "settings-save-button", text = "Save", type = ButtonType.SUBMIT).onClick {
+                    when (chooises) {
+                        0 -> {
                             console.log("chooises is $chooises")
                             window.localStorage.setItem("defaultLeaderboardType", LeaderboardsView.VANILA.toString())
                         }
-                        1 ->{
+
+                        1 -> {
                             console.log("chooises is $chooises")
                             window.localStorage.setItem("defaultLeaderboardType", LeaderboardsView.RELAX.toString())
                         }
-                        2 ->{
+
+                        2 -> {
                             console.log("chooises is $chooises")
                             window.localStorage.setItem("defaultLeaderboardType", LeaderboardsView.AUTOPILOT.toString())
                         }
                     }
+                    ToastContainer(ToastContainerPosition.BOTTOMRIGHT).showToast(
+                        message = "Default mode saved",
+                        title ="Setting",
+                        delay = 2000
+                    )
+                    this.style = ButtonStyle.SUCCESS
+                    this.text = "Saved"
                 }
             }
 
